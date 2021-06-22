7523c43ef9495b5b08fa528708cfac2bd00583cc
AbubakrAlameri <62396017+abubakrAlameri@users.noreply.github.com>
2021-06-22 19:59:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+   
+  define('DS',DIRECTORY_SEPARATOR);
+  define('ROOT',dirname(__FILE__));
+
+  require_once(ROOT . DS . 'config' . DS . 'config.php');
+  require_once(ROOT . DS . 'app' . DS . 'helpers' . DS . 'functions.php');
+  require( ROOT .DS.'vendor' .DS .'autoload.php');
+  // this function autoloading the autoload function from helpers/functions.php
+  spl_autoload_register('autoload'); 
+
+
+  session_start();
+if (!Session::exists(GUEST))
+  Session::set(GUEST, $_SERVER['REMOTE_ADDR']);
+
+
+Router::route();
+
+
+   
